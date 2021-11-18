08e415dbc9063c5a1fc0d7bbe36b64073db3f94a
Andrew <andrewrpelton@outlook.com>
2021-11-17 19:57:23 -0800

    Added a Player Controller and Renderer

---
+++ code/renderer.py
@@ -0,0 +1,71 @@
+import pygame
+import math
+import sys
+
+#classes
+class Player:
+    def __init__(self):
+        self.x = 10
+        self.y = -10
+        self.speed = 10
+        self.size = 20
+
+    def move(self):
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.x += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.x -= self.speed
+        elif keys[pygame.K_UP]:
+            self.y -= self.speed
+        elif keys[pygame.K_DOWN]:
+            self.y += self.speed
+
+        if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
+            self.x -= self.speed / 2
+            self.y -= self.speed / 2 
+        elif keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
+            self.x -= self.speed / 2
+            self.y += self.speed / 2
+        elif keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
+           self.x += self.speed / 2
+           self.y -= self.speed / 2
+        elif keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
+            self.x += self.speed / 2
+            self.y += self.speed / 2
+        
+        
+        
+            
+
+    def render(self):
+        pygame.draw.rect(window, (13, 170, 255), (self.x, self.y, self.size, self.size))
+
+
+
+
+
+#Setup Window
+window = pygame.display.set_mode((750, 750))
+pygame.display.set_caption('Pygame')
+clock = pygame.time.Clock()
+
+#Create Objects
+p = Player()
+  
+#Game Loop
+while True:
+    #Refresh
+    pygame.display.update()
+    msElapsed = clock.tick(30)
+
+    #Player
+    window.fill((0, 0, 0))
+    p.move()      
+    p.render()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
